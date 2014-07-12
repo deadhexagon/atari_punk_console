a440311ccb544d94519a10b1c7162538556455bc
Steven Cook <visage@deadhexagon.com>
2014-07-11 23:58:53 -0400

    Initial project import

---
+++ atari_punk_console.cmd
@@ -0,0 +1,85 @@
+# Pin name action command file
+
+# Start of element D2
+ChangePinName(D2, 2, 2)
+ChangePinName(D2, 1, 1)
+
+# Start of element J1
+ChangePinName(J1, 2, 2)
+ChangePinName(J1, 3, 3)
+ChangePinName(J1, 1, 1)
+
+# Start of element S1
+ChangePinName(S1, 1, 1)
+ChangePinName(S1, 2, 2)
+
+# Start of element B1
+ChangePinName(B1, 2, -)
+ChangePinName(B1, 1, +)
+
+# Start of element D1
+ChangePinName(D1, 2, 2)
+ChangePinName(D1, 1, 1)
+
+# Start of element R7
+ChangePinName(R7, 1, 1)
+ChangePinName(R7, 2, 2)
+
+# Start of element C3
+ChangePinName(C3, 2, -)
+ChangePinName(C3, 1, +)
+
+# Start of element C2
+ChangePinName(C2, 2, PIN2)
+ChangePinName(C2, 1, PIN1)
+
+# Start of element C1
+ChangePinName(C1, 2, PIN2)
+ChangePinName(C1, 1, PIN1)
+
+# Start of element SPK1
+ChangePinName(SPK1, 2, NEG)
+ChangePinName(SPK1, 1, POS)
+
+# Start of element R6
+ChangePinName(R6, 1, 1)
+ChangePinName(R6, 3, 3)
+ChangePinName(R6, 2, 2)
+
+# Start of element R2
+ChangePinName(R2, 1, 1)
+ChangePinName(R2, 3, 3)
+ChangePinName(R2, 2, 2)
+
+# Start of element R1
+ChangePinName(R1, 1, 1)
+ChangePinName(R1, 3, 3)
+ChangePinName(R1, 2, 2)
+
+# Start of element R5
+ChangePinName(R5, 1, 1)
+ChangePinName(R5, 2, 2)
+
+# Start of element R3
+ChangePinName(R3, 1, 1)
+ChangePinName(R3, 2, 2)
+
+# Start of element R4
+ChangePinName(R4, 1, 1)
+ChangePinName(R4, 2, 2)
+
+# Start of element U1
+ChangePinName(U1, 8, TRG B)
+ChangePinName(U1, 9, OUT B)
+ChangePinName(U1, 10, \_RESET\_ B)
+ChangePinName(U1, 11, CTL B)
+ChangePinName(U1, 12, THR B)
+ChangePinName(U1, 13, DIS B)
+ChangePinName(U1, 14, VCC)
+ChangePinName(U1, 7, GND)
+ChangePinName(U1, 6, TRG A)
+ChangePinName(U1, 5, OUT A)
+ChangePinName(U1, 4, \_RESET\_ A)
+ChangePinName(U1, 3, CTL A)
+ChangePinName(U1, 2, THR A)
+ChangePinName(U1, 1, DIS A)
